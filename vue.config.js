48e9d4ec8a16e06b5377f803060ca7bf6f0bfe93
Ltotheuhas <lkokorovic@hotmail.com>
2023-11-01 14:19:47 +0100

    test?

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === "production" ? "/Ltotheuhas.github.io/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/newsite/" : "/",
   transpileDependencies: true,
 
   pluginOptions: {
