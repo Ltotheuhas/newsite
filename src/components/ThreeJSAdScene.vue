338c7d86e8494059a527bcbdb3d36cbdd67aac5c
Ltotheuhas <lkokorovic@hotmail.com>
2024-10-27 20:24:54 +0100

    idk dude

--- src/components/ThreeJSAdScene.vue
+++ src/components/ThreeJSAdScene.vue
@@ -4,15 +4,6 @@
 
 <script>
 import * as THREE from 'three';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
-import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js';
-import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader.js';
-import { STLLoader } from 'three/examples/jsm/loaders/STLLoader.js';
-import { ColladaLoader } from 'three/examples/jsm/loaders/ColladaLoader.js';
-import { TDSLoader } from 'three/examples/jsm/loaders/TDSLoader.js';
-import { PLYLoader } from 'three/examples/jsm/loaders/PLYLoader.js';
-// import { X3DLoader } from 'three/examples/jsm/loaders/X3DLoader.js';
-import { VRMLLoader } from 'three/examples/jsm/loaders/VRMLLoader.js';
 import { parseGIF, decompressFrames } from 'gifuct-js';
 
 export default {
@@ -38,15 +29,19 @@ export default {
             // Setup
             const container = this.$refs.threeContainer;
             const scene = new THREE.Scene();
-            const randomFOV = Math.random() * 50 + 100;
+            const randomFOV = Math.random() * 30 + 80;
             const camera = new THREE.PerspectiveCamera(randomFOV, window.innerWidth / window.innerHeight, 0.1, 1000);
             const renderer = new THREE.WebGLRenderer();
 
             const radius = 5;
-            let angle = 0;
+            let angle = Math.random() * Math.PI * 2;
             const rotationSpeed = 0.005;
 
-            const randomYPosition = Math.random() * 9 + 1;
+            const randomFrequency = Math.random() * 2 + 0.5;
+            const amplitude = 2;
+            let time = 0;
+            const randomYPosition = Math.random() * 4 + 1;
+
             camera.position.set(Math.cos(angle) * radius, randomYPosition, Math.sin(angle) * radius);
             camera.lookAt(0, 0, 0);
 
@@ -73,26 +68,28 @@ export default {
                 const planeGeometry = new THREE.PlaneGeometry(4, 4 / aspect);
                 const planeMaterial = new THREE.MeshBasicMaterial({ map: texture, transparent: true, side: THREE.DoubleSide });
                 const plane = new THREE.Mesh(planeGeometry, planeMaterial);
-                plane.position.set(0, 4, 0);
+                plane.position.set(0, 2, 0);
                 scene.add(plane);
 
                 // Animation loop for rotating the image plane
-                const animate = () => {
-                    requestAnimationFrame(animate);
-
+                const animatePlane = () => {
+                    requestAnimationFrame(animatePlane);
                     plane.rotation.y += 0.01;
                 };
-                animate();
+                animatePlane();
             });
 
+            // Animation loop for rotating the camera
             const animate = () => {
                 requestAnimationFrame(animate);
 
+                time += rotationSpeed;
                 angle += rotationSpeed;
                 camera.position.x = Math.cos(angle) * radius;
                 camera.position.z = Math.sin(angle) * radius;
-                camera.lookAt(0, 0, 0);
+                camera.position.y = randomYPosition + Math.sin(time * randomFrequency) * amplitude; // Apply sine wave to Y position
 
+                camera.lookAt(0, 0, 0);
                 renderer.render(scene, camera);
             };
             animate();
@@ -105,57 +102,67 @@ export default {
             const apiUrl = process.env.VUE_APP_API_URL;
             try {
                 const response = await fetch(`${apiUrl}/objects`);
-
                 const objects = await response.json();
 
-                const objectPromises = objects.map(obj => {
-                    if (obj.type === 'image') {
-                        return this.loadImageFromData(obj);
-                    } else if (obj.type === 'gif') {
-                        return this.loadGIFFromData(obj);
-                    } else if (obj.type === 'model') {
-                        return this.loadModelFromData(obj);
-                    }
-                });
-                await Promise.all(objectPromises);
+                objects.sort((a, b) => this.getDistanceFromOrigin(a) - this.getDistanceFromOrigin(b));
+
+                const batchSize = 5;
+                const delayBetweenBatches = 500;
+
+                for (let i = 0; i < objects.length; i += batchSize) {
+                    const batch = objects.slice(i, i + batchSize);
+                    const objectPromises = batch.map(obj => {
+                        if (obj.type === 'image') {
+                            return this.loadImageFromData(obj);
+                        } else if (obj.type === 'gif') {
+                            return this.loadGIFFromData(obj);
+                        }
+                    });
+
+                    await Promise.all(objectPromises);
+                    await new Promise(resolve => setTimeout(resolve, delayBetweenBatches));
+                }
             } catch (error) {
                 console.error('Error loading objects from backend:', error);
             }
         },
+        getDistanceFromOrigin(obj) {
+            if (obj.position) {
+                const { x, y, z } = obj.position;
+                return Math.sqrt(x * x + y * y + z * z);
+            }
+            return Infinity;
+        },
         loadImageFromData(obj) {
             const textureLoader = new THREE.TextureLoader();
+            // Use the '-small.webp' version of the image
+            const imageUrl = `${process.env.VUE_APP_API_URL}${obj.filePath.replace(/\.[^/.]+$/, '-small.webp')}`;
 
-            if (obj.filePath) {
-                textureLoader.load(
-                    `${process.env.VUE_APP_API_URL}${obj.filePath}`,
-                    (texture) => {
-                        // Check if texture image is loaded properly
-                        if (texture.image && texture.image.width && texture.image.height) {
-                            const aspect = texture.image.width / texture.image.height;
-                            const scale = obj.scale || 2; // Optionally use 'scale' property from obj, default to 2
+            textureLoader.load(
+                imageUrl,
+                (texture) => {
+                    const aspect = texture.image.width / texture.image.height;
+                    const scale = obj.scale || 2;
 
-                            // Create plane geometry and material using the texture
-                            const planeGeometry = new THREE.PlaneGeometry(scale, scale / aspect);
-                            const planeMaterial = new THREE.MeshBasicMaterial({
-                                map: texture,
-                                side: THREE.DoubleSide,
-                                transparent: true // Enable transparency
-                            });
+                    const planeGeometry = new THREE.PlaneGeometry(scale, scale / aspect);
+                    const planeMaterial = new THREE.MeshBasicMaterial({
+                        map: texture,
+                        side: THREE.DoubleSide,
+                        transparent: true
+                    });
 
-                            // Create and configure the plane mesh
-                            const plane = new THREE.Mesh(planeGeometry, planeMaterial);
-                            plane.position.copy(obj.position || new THREE.Vector3(0, 0, 0)); // Default position if not set
-                            plane.rotation.copy(obj.rotation || new THREE.Euler(0, 0, 0)); // Default rotation if not set
+                    const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+                    plane.position.copy(obj.position || new THREE.Vector3(0, 0, 0));
+                    plane.rotation.copy(obj.rotation || new THREE.Euler(0, 0, 0));
 
-                            this.scene.add(plane); // Add the plane to the scene
-
-                            // Save the object reference for future cleanup
-                            this.objects.push(plane);
-                        }
-                    },
-                    undefined
-                );
-            }
+                    this.scene.add(plane);
+                    this.objects.push(plane);
+                },
+                undefined,
+                (err) => {
+                    console.error('Error loading image texture:', err);
+                }
+            );
         },
         async loadGIFFromData(obj) {
             const response = await fetch(`${process.env.VUE_APP_API_URL}${obj.filePath}`);
@@ -213,75 +220,6 @@ export default {
             };
 
             animateGIF();
-        },
-        async loadModelFromData(obj) {
-            // Extract the file extension from the filePath
-            const filePath = obj.filePath;
-            const extension = filePath.split('.').pop().toLowerCase(); // Extract extension after last dot
-
-            const loader = this.getLoader(extension);
-            if (!loader) {
-                return;
-            }
-
-            const url = `${process.env.VUE_APP_API_URL}${obj.filePath}`;
-
-            return new Promise((resolve, reject) => {
-                loader.load(
-                    url,
-                    (gltf) => {
-                        let sceneObject = gltf.scene ? gltf.scene : gltf;
-
-                        // Set position and rotation
-                        sceneObject.position.set(obj.position.x, obj.position.y, obj.position.z);
-                        sceneObject.rotation.set(obj.rotation._x, obj.rotation._y, obj.rotation._z, obj.rotation._order);
-                        sceneObject.uuid = obj.uuid;
-
-                        // Calculate bounding box and limit the height
-                        const boundingBox = new THREE.Box3().setFromObject(sceneObject);
-                        const modelHeight = boundingBox.max.y - boundingBox.min.y;
-                        const maxHeight = 5; // Set your desired maximum height here
-
-                        // Scale down if the model's height exceeds maxHeight
-                        if (modelHeight > maxHeight) {
-                            const scale = maxHeight / modelHeight;
-                            sceneObject.scale.set(scale, scale, scale);
-                        }
-
-                        this.scene.add(sceneObject);
-                        resolve(sceneObject);
-                    },
-                    undefined,
-                    (error) => {
-                        reject(error);
-                    }
-                );
-            });
-        },
-        getLoader(extension) {
-            switch (extension.toLowerCase()) {
-                case 'gltf':
-                case 'glb':
-                    return new GLTFLoader();
-                case 'obj':
-                    return new OBJLoader();
-                case 'fbx':
-                    return new FBXLoader();
-                case 'stl':
-                    return new STLLoader();
-                case 'dae':
-                    return new ColladaLoader();
-                case '3ds':
-                    return new TDSLoader();
-                case 'ply':
-                    return new PLYLoader();
-                /*case 'x3d':
-                  return new X3DLoader();*/
-                case 'wrl':
-                    return new VRMLLoader();
-                default:
-                    return null;
-            }
         }
     }
 };
