ce39a2bdf9214e673da2a4bc307888b13768b271
Ltotheuhas <lkokorovic@hotmail.com>
2024-11-03 12:28:57 +0100

    deploy 9

--- src/components/ProductComp.vue
+++ src/components/ProductComp.vue
@@ -1,35 +1,28 @@
 <template>
     <v-container>
-      <h2>{{ product.name }}</h2>
-      <p>{{ product.description }}</p>
-      <p>Price: ${{ product.price }}</p>
-      <button
-        class="snipcart-add-item"
-        :data-item-id="product.id"
-        :data-item-name="product.name"
-        :data-item-price="product.price"
-        :data-item-url="product.url"
-        :data-item-description="product.description"
-      >
-        Add to Cart
-      </button>
+        <h2>{{ product.name }}</h2>
+        <p>{{ product.description }}</p>
+        <p>Price: ${{ product.price }}</p>
+        <button class="snipcart-add-item" :data-item-id="product.id" :data-item-name="product.name"
+            :data-item-price="product.price" :data-item-url="product.url" :data-item-description="product.description">
+            Add to Cart
+        </button>
     </v-container>
-  </template>
-  
-  <script>
-  export default {
+</template>
+
+<script>
+export default {
     name: 'ProductComp',
     data() {
-      return {
-        product: {
-          id: 'test-product',
-          name: 'Test Product',
-          price: '0.00',
-          url: `https://luhas.gratis/store/test-product`,
-          description: 'This is a test product description.'
-        }
-      };
+        return {
+            product: {
+                id: 'test-product',
+                name: 'Test Product',
+                price: '10.00',  // Set the intended price here
+                url: `https://luhas.gratis/store/test-product`,
+                description: 'This is a test product description.'
+            }
+        };
     }
-  };
-  </script>
-  
+};
+</script>
