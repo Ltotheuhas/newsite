fa58612dc52893bc9079c1569033f4f40a21bec8
Ltotheuhas <lkokorovic@hotmail.com>
2024-11-24 15:05:41 +0100

    errrrm whatevers

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -201,16 +201,16 @@ export default {
             if (!window.hcb_user) {
                 window.hcb_user = {};
             }
+
+            const cleanUrl = window.location.href.split('?')[0];
             const s = document.createElement('script');
-            const l = window.hcb_user.PAGE || ("" + window.location).replace(/'/g, "%27");
+            const l = window.hcb_user.PAGE || cleanUrl.replace(/'/g, "%27");
             const h = "https://www.htmlcommentbox.com";
 
             s.setAttribute("type", "text/javascript");
             s.setAttribute(
                 "src",
-                h + "/jread?page=" + encodeURIComponent(l).replace("+", "%2B") +
-                "&mod=%241%24wq1rdBcg%24jJ44Z9Yty1bqAkTYA64Nz1" +
-                "&opts=16798&num=10&ts=" + Date.now()
+                `${h}/jread?page=${encodeURIComponent(l).replace("+", "%2B")}&mod=%241%24wq1rdBcg%24jJ44Z9Yty1bqAkTYA64Nz1&opts=16798&num=10&ts=${Date.now()}`
             );
 
             if (typeof s != "undefined") {
